e2f029ed5ae41ce05733367103557007d2620499
Link-Liu <2256981152@qq.com>
2024-12-29 21:44:17 +0800

    finish lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -74,6 +74,6 @@ public class CapersRepository {
     public static void celebrateBirthday(String name) {
         // TODO
         Dog dog = Dog.fromFile(name);
-        dog.saveDog();
+        dog.haveBirthday();
     }
 }
