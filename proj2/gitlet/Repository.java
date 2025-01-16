aba703a9729d1ddde71ccf306807982890ccc188
Link-Liu <2256981152@qq.com>
2025-01-16 15:17:47 +0800

    fix log

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -22,9 +22,13 @@ public class Repository {
      *
      */
 
-    /** The current working directory. */
+    /**
+     * The current working directory.
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
+    /**
+     * The .gitlet directory.
+     */
     public static final File GITLET_DIR = join(CWD, ".gitlet"); //dir
     public static final File OBJECT_DIR = join(GITLET_DIR, "object"); //dir
     public static final File STAGE_DIR = join(GITLET_DIR, "stage");  //file
@@ -37,7 +41,7 @@ public class Repository {
     public static void gitInit() {
         if (GITLET_DIR.exists()) {
             String msg;
-            msg =  "A Gitlet version-control system already exists in the current directory.";
+            msg = "A Gitlet version-control system already exists in the current directory.";
             System.out.println(msg);
         } else {
             GITLET_DIR.mkdir();
@@ -49,6 +53,7 @@ public class Repository {
             new Commit();
         }
     }
+
     /*把文件加入stagingAdd*/
     public static void gitAdd(String fileName) {
         File file = join(CWD, fileName);
@@ -111,202 +116,197 @@ public class Repository {
     }
 
     public static void gitGlobalLog() {
-        List<String> log = Utils.plainFilenamesIn(COMMIT_DIR);
-        StringBuilder builder = new StringBuilder();
-        List<String> commitList = plainFilenamesIn(OBJECT_DIR);
+        List<String> commitList = plainFilenamesIn(COMMIT_DIR);
         Commit commit;
         for (String id : commitList) {
-            try {
-                commit = Commit.load(id);
-                if (commit.have2Parent()) {
-                    printMergeCommit(commit);
-                } else {
-                    printCommit(commit);
-                }
-            } catch (Exception ignore) {
-            }
-        }
-    }
-
-    public static void gitFind(String massage) {
-        List<String> log = Utils.plainFilenamesIn(COMMIT_DIR);
-        StringBuilder builder = new StringBuilder();
-        for (String s: log) {
-            Commit curCommit = Commit.load(s);
-            String curMasssage = curCommit.getMessage();
-            if (curMasssage.equals(massage)) {
-                builder.append(s).append("\n");
-            }
-        }
-        if (builder.length() == 0) {
-            System.out.println("Found no commit with that message.");
-        } else {
-            System.out.print(builder.toString());
-        }
-    }
-
-    public static void gitStatus() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(Head.printBranch());
-        builder.append(Stage.printStages());
-        builder.append(ModificationChecker.printCwdfiles());
-        builder.append("=== Untracked Files ===").append(System.lineSeparator());
-        List<String> untrackedFiles = Commit.getUntrackedFileName();
-        for (String s: untrackedFiles) {
-            builder.append(s);
-            builder.append(System.lineSeparator());
-        }
-        System.out.println(builder.toString());
-    }
-
-    public static void gitCheckout2(String branchName) { //branchName是一个名字
-        TreeMap<String, String> branchs = readObject(HEAD, Head.class).getBranch();
-        if (!branchs.containsKey(branchName)) {
-            System.out.println("No such branch exists.");
-            return;
-        }
-        if (Head.getCurHead().equals(branchName)) {
-            System.out.println("No need to checkout the current branch.");
-            return;
-        }
-        String hash = branchs.get(branchName);
-        setBranch(hash);
-        Head.checkoutBranch(branchName);
-    }
-
-    /*从head找文件*/
-    public static void gitCheckout3(String fileName) {
-        String lookedId = Head.getCurHead();
-        gitCheckout4(lookedId, fileName);
-    }
-
-    /*从特定commit找文件*/
-    public static void gitCheckout4(String commitId, String fileName) {
-        if (commitId.length() < MINCOMMITSIZE) {
-            commitId = findAllHash(commitId);
-        }
-        List<String> log = plainFilenamesIn(COMMIT_DIR);
-        if (!log.contains(commitId)) {
-            System.out.println("No commit with that id exists.");
-            return;
-        }
-        Commit lookedCommit = Commit.load(commitId);
-        TreeMap<String, String> referenceMap = lookedCommit.getBlobTreeMap();
-        if (referenceMap.containsKey(fileName)) {
-            String lookedHash = referenceMap.get(fileName);
-            Blob lookedBlob = Blob.load(lookedHash);
-            byte[] lookedContents = lookedBlob.getContents();
-            File fileToCheckout = join(CWD, fileName);
-            writeContents(fileToCheckout, (Object) lookedContents);
-        } else {
-            System.out.println("File does not exist in that commit.");
-        }
-    }
-
-    public static void gitBranch(String branchName) {
-        Head.createBranch(branchName);
-    }
-
-    public static void rmBranch(String branchName) {
-        Head.removeBranch(branchName);
-    }
-
-    public static void reset(String commitId) {
-        List<String> log = Utils.plainFilenamesIn(COMMIT_DIR);
-        if (!log.contains(commitId)) {
-            System.out.println("No commit with that id exists.");
-        } else {
-            setBranch(commitId);
-            Stage stage = Stage.load();
-            stage.clear();
-            stage.save();
-            Head.setId(commitId);
-        }
-    }
-
-    public static void setBranch(String commitHash) {
-        if (!Commit.getUntrackedFileName().isEmpty()) {
-            String massage = "There is an untracked file in the way; ";
-            System.out.println(massage + "delete it, or add and commit it first.");
-            return;
-        }
-        List<String> cwdFileNames = Utils.plainFilenamesIn(CWD);
-        Commit branchCommit = Commit.load(commitHash);
-        TreeMap<String, String> blobMap = branchCommit.getBlobTreeMap();
-        for (String fileName: cwdFileNames) {
-            if (!blobMap.containsKey(fileName)) {
-                restrictedDelete(join(CWD, fileName));
-            }
-        }
-        for (Map.Entry<String, String> entry : blobMap.entrySet()) {
-            String name = entry.getKey();
-            String hash = entry.getValue();
-            byte[] blobContents = Blob.load(hash).getContents();
-            writeContents(join(CWD, name), blobContents);
-        }
-    }
-
-    public static void checkInit() {
-        if (!(GITLET_DIR.exists() && GITLET_DIR.isDirectory())) {
-            System.out.println("Not in an initialized Gitlet directory.");
-            System.exit(0);
-        }
-    }
-
-    private static String findAllHash(String shortHash) {
-        if (shortHash.length() < 6) {
-            System.out.println("Short hash is too short to find hash.");
-            return shortHash;
-        } else {
-            List<String> allCommitIds = plainFilenamesIn(COMMIT_DIR);
-            int count = 0;
-            String hash = shortHash;
-            for (String commitId: allCommitIds) {
-                if (commitId.startsWith(shortHash)) {
-                    count++;
-                    hash = commitId;
-                }
-            }
-            if (count == 1) {
-                return hash;
+            commit = Commit.load(id);
+            if (commit.have2Parent()) {
+                printMergeCommit(commit);
             } else {
-                System.out.println("More than one commit with that id exists.");
+                printCommit(commit);
+            }
+        }
+    }
+
+        public static void gitFind (String massage){
+            List<String> log = Utils.plainFilenamesIn(COMMIT_DIR);
+            StringBuilder builder = new StringBuilder();
+            for (String s : log) {
+                Commit curCommit = Commit.load(s);
+                String curMasssage = curCommit.getMessage();
+                if (curMasssage.equals(massage)) {
+                    builder.append(s).append("\n");
+                }
+            }
+            if (builder.length() == 0) {
+                System.out.println("Found no commit with that message.");
+            } else {
+                System.out.print(builder.toString());
+            }
+        }
+
+        public static void gitStatus () {
+            StringBuilder builder = new StringBuilder();
+            builder.append(Head.printBranch());
+            builder.append(Stage.printStages());
+            builder.append(ModificationChecker.printCwdfiles());
+            builder.append("=== Untracked Files ===").append(System.lineSeparator());
+            List<String> untrackedFiles = Commit.getUntrackedFileName();
+            for (String s : untrackedFiles) {
+                builder.append(s);
+                builder.append(System.lineSeparator());
+            }
+            System.out.println(builder.toString());
+        }
+
+        public static void gitCheckout2 (String branchName){ //branchName是一个名字
+            TreeMap<String, String> branchs = readObject(HEAD, Head.class).getBranch();
+            if (!branchs.containsKey(branchName)) {
+                System.out.println("No such branch exists.");
+                return;
+            }
+            if (Head.getCurHead().equals(branchName)) {
+                System.out.println("No need to checkout the current branch.");
+                return;
+            }
+            String hash = branchs.get(branchName);
+            setBranch(hash);
+            Head.checkoutBranch(branchName);
+        }
+
+        /*从head找文件*/
+        public static void gitCheckout3 (String fileName){
+            String lookedId = Head.getCurHead();
+            gitCheckout4(lookedId, fileName);
+        }
+
+        /*从特定commit找文件*/
+        public static void gitCheckout4 (String commitId, String fileName){
+            if (commitId.length() < MINCOMMITSIZE) {
+                commitId = findAllHash(commitId);
+            }
+            List<String> log = plainFilenamesIn(COMMIT_DIR);
+            if (!log.contains(commitId)) {
+                System.out.println("No commit with that id exists.");
+                return;
+            }
+            Commit lookedCommit = Commit.load(commitId);
+            TreeMap<String, String> referenceMap = lookedCommit.getBlobTreeMap();
+            if (referenceMap.containsKey(fileName)) {
+                String lookedHash = referenceMap.get(fileName);
+                Blob lookedBlob = Blob.load(lookedHash);
+                byte[] lookedContents = lookedBlob.getContents();
+                File fileToCheckout = join(CWD, fileName);
+                writeContents(fileToCheckout, (Object) lookedContents);
+            } else {
+                System.out.println("File does not exist in that commit.");
+            }
+        }
+
+        public static void gitBranch (String branchName){
+            Head.createBranch(branchName);
+        }
+
+        public static void rmBranch (String branchName){
+            Head.removeBranch(branchName);
+        }
+
+        public static void reset (String commitId){
+            List<String> log = Utils.plainFilenamesIn(COMMIT_DIR);
+            if (!log.contains(commitId)) {
+                System.out.println("No commit with that id exists.");
+            } else {
+                setBranch(commitId);
+                Stage stage = Stage.load();
+                stage.clear();
+                stage.save();
+                Head.setId(commitId);
+            }
+        }
+
+        public static void setBranch (String commitHash){
+            if (!Commit.getUntrackedFileName().isEmpty()) {
+                String massage = "There is an untracked file in the way; ";
+                System.out.println(massage + "delete it, or add and commit it first.");
+                return;
+            }
+            List<String> cwdFileNames = Utils.plainFilenamesIn(CWD);
+            Commit branchCommit = Commit.load(commitHash);
+            TreeMap<String, String> blobMap = branchCommit.getBlobTreeMap();
+            for (String fileName : cwdFileNames) {
+                if (!blobMap.containsKey(fileName)) {
+                    restrictedDelete(join(CWD, fileName));
+                }
+            }
+            for (Map.Entry<String, String> entry : blobMap.entrySet()) {
+                String name = entry.getKey();
+                String hash = entry.getValue();
+                byte[] blobContents = Blob.load(hash).getContents();
+                writeContents(join(CWD, name), blobContents);
+            }
+        }
+
+        public static void checkInit () {
+            if (!(GITLET_DIR.exists() && GITLET_DIR.isDirectory())) {
+                System.out.println("Not in an initialized Gitlet directory.");
                 System.exit(0);
             }
         }
-        return shortHash;
-    }
 
-    private static void printCommit(Commit currCommmit) {
-        System.out.println("===");
-        printCommitID(currCommmit);
-        printCommitDate(currCommmit);
-        printCommitMessage(currCommmit);
-    }
+        private static String findAllHash (String shortHash){
+            if (shortHash.length() < 6) {
+                System.out.println("Short hash is too short to find hash.");
+                return shortHash;
+            } else {
+                List<String> allCommitIds = plainFilenamesIn(COMMIT_DIR);
+                int count = 0;
+                String hash = shortHash;
+                for (String commitId : allCommitIds) {
+                    if (commitId.startsWith(shortHash)) {
+                        count++;
+                        hash = commitId;
+                    }
+                }
+                if (count == 1) {
+                    return hash;
+                } else {
+                    System.out.println("More than one commit with that id exists.");
+                    System.exit(0);
+                }
+            }
+            return shortHash;
+        }
 
-    private static void printMergeCommit(Commit currCommmit) {
-        System.out.println("===");
-        printCommitID(currCommmit);
-        printMergeMark(currCommmit);
-        printCommitDate(currCommmit);
-        printCommitMessage(currCommmit);
-    }
+        private static void printCommit (Commit currCommmit){
+            System.out.println("===");
+            printCommitID(currCommmit);
+            printCommitDate(currCommmit);
+            printCommitMessage(currCommmit);
+        }
 
-    private static void printCommitID(Commit currCommmit) {
-        System.out.println("commit " + currCommmit.getId());
-    }
+        private static void printMergeCommit (Commit currCommmit){
+            System.out.println("===");
+            printCommitID(currCommmit);
+            printMergeMark(currCommmit);
+            printCommitDate(currCommmit);
+            printCommitMessage(currCommmit);
+        }
 
-    private static void printMergeMark(Commit currCommmit) {
-        String parent1 = currCommmit.getParentId();
-        String parent2 = currCommmit.getParentId2();
-        System.out.println("Merge: " + parent1.substring(0, 7) + " " + parent2.substring(0, 7));
-    }
+        private static void printCommitID (Commit currCommmit){
+            System.out.println("commit " + currCommmit.getId());
+        }
 
-    private static void printCommitDate(Commit currCommmit) {
-        System.out.println("Date: " + currCommmit.getTimeStamp());
-    }
+        private static void printMergeMark (Commit currCommmit){
+            String parent1 = currCommmit.getParentId();
+            String parent2 = currCommmit.getParentId2();
+            System.out.println("Merge: " + parent1.substring(0, 7) + " " + parent2.substring(0, 7));
+        }
 
-    private static void printCommitMessage(Commit currCommmit) {
-        System.out.println(currCommmit.getMessage() + "\n");
-    }
+        private static void printCommitDate (Commit currCommmit){
+            System.out.println("Date: " + currCommmit.getTimeStamp());
+        }
+
+        private static void printCommitMessage (Commit currCommmit){
+            System.out.println(currCommmit.getMessage() + "\n");
+        }
 }
